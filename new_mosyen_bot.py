3659b160c3162342d602085038d06c2695962240
andreesc15 <37733902+andreesc15@users.noreply.github.com>
2019-10-26 09:02:32 +0700

    Update new_mosyen_bot.py

--- new_mosyen_bot.py
+++ new_mosyen_bot.py
@@ -10,17 +10,38 @@ access_token_secret ="xaibDbqfuDIrMQmGtwNZ2IB5scS7fqIaibFNL916XqliJ"
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
-
-actors = [n[:-1] for n in open("actor.txt","r").readlines()]
-events = [n[:-1] for n in open("event.txt","r").readlines()]
-objects = [n[:-1] for n in open("object.txt","r").readlines()]
+#--master database
+_actors = [n[:-1] for n in open("actor.txt","r").readlines()]
+_events = [n[:-1] for n in open("event.txt","r").readlines()]
+_objects = [n[:-1] for n in open("object.txt","r").readlines()]
 templates = [n[:-1] for n in open("template.txt","r").readlines()]
+#--end of master database
+
+#--secondary database
+actors = [n for n in _actors]
+events = [n for n in _events]
+objects = [n for n in _objects]
 
 while True:
 	try:
-		motion = choice(templates).replace("<actors>",choice(actors)).replace("<events>",choice(events)).replace("<objects>",choice(objects))
+		if len(actors) == 0:
+			actors.extend(_actors)
+		if len(events) == 0:
+			events.extend(_events)
+		if len(objects) == 0:
+			objects.extend(_objects)
+
+		curr_actor = choice(actors)
+		curr_event = choice(events)
+		curr_object = choice(objects)
+
+		actors.remove(curr_actor)
+		events.remove(curr_event)
+		objects.remove(curr_object)
+
+		motion = choice(templates).replace("<actors>",curr_actor).replace("<events>",curr_event).replace("<objects>",curr_object)
 		api.update_status(motion)
 		print(motion)
-		time.sleep(3600)
-	except KeyboardInterrupt:
-		break
+		time.sleep(1800)
+	except TweepyError:
+		continue
